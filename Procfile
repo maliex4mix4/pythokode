641950e9845542fc68a82ac4a381b844f82b2ab1
Maliex <malikopeyemi5@gmail.com>
2020-05-25 06:12:54 +0100

    Revert "2nd commit" This reverts commit 0b2e29af2becf541cc347dbcd535334c2acc510c.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn pythokode.wsgi --log-file -
--- pythokode/settings.py
+++ pythokode/settings.py
@@ -25,7 +25,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = '5jwzt7k_^tqii69y5-vs67u&eb$7d$r=ztiueyp!*^vo_wq#$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -46,9 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -88,17 +85,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-
-DATABASES['default'].update(db_from_env)
-
-import django_heroku
-
-django_heroku.settings(locals())
-
-#del DATABASES['default']['OPTIONS']['sslmode']
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -137,12 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGOUT_REDIRECT_URL = '/users/login'
 LOGIN_REDIRECT_URL = '/code'
